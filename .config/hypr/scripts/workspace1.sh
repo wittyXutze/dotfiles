fb3cf42fc2fea0a493eb36b5f11486357969119f
withold <withold.schuetze@gmx.de>
2026-06-26 21:33:20 +0200

    workspace1: fix brave-browser class

--- .config/hypr/scripts/workspace1.sh
+++ .config/hypr/scripts/workspace1.sh
@@ -11,4 +11,4 @@ sleep 0.5
 hyprctl dispatch workspace $WS
 hyprctl dispatch exec "[workspace $WS silent; float; size 1517 1672; move 12 44] kitty"
 sleep 0.5
-hyprctl dispatch exec "[workspace $WS silent; float; size 1517 1672; move 1543 44] brave"
+hyprctl dispatch exec "[workspace $WS silent; float; size 1517 1672; move 1543 44] brave-browser"
