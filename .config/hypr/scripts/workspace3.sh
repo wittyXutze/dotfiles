8eef76707060c88a4ef6d1b3537d5f52d9d957b9
withold <withold.schuetze@gmx.de>
2026-06-27 10:51:18 +0200

    workspace3: sleep 0.5 vor nautilus, loop für thunderbird

--- .config/hypr/scripts/workspace3.sh
+++ .config/hypr/scripts/workspace3.sh
@@ -7,10 +7,10 @@ for x in json.load(sys.stdin):
 " | while read addr; do
     hyprctl dispatch closewindow address:$addr
 done
-sleep 0.5
+sleep 0.2
 hyprctl dispatch workspace $WS
 hyprctl dispatch exec "[workspace $WS silent; float; size 1142 833; move 1 42] nautilus --new-window /home/withold"
-sleep 2
+sleep 0.2
 hyprctl dispatch focuswindow class:org.gnome.Nautilus
 hyprctl dispatch moveactive exact -78 88
 hyprctl dispatch resizeactive exact 1142 833
